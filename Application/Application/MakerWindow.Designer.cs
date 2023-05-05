833781973e88051778c3e7339e561972ab6a9863
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-05-05 14:08:47 -0400

    Init

---
+++ Application/Application/MakerWindow.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace App
+{
+    partial class MakerWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MakerWindow));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.TextBox();
+            this.compilebtn = new System.Windows.Forms.Button();
+            this.description = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.credittitle = new System.Windows.Forms.Label();
+            this.makerzonetitle = new System.Windows.Forms.Label();
+            this.ground = new System.Windows.Forms.Label();
+            this.primaryatlas = new System.Windows.Forms.Label();
+            this.secondaryatlas = new System.Windows.Forms.Label();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.forestatlaspriview = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.isverfiedbtn = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.forestatlaspriview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.Controls.Add(this.isverfiedbtn);
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Controls.Add(this.compilebtn);
+            this.panel1.Controls.Add(this.description);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.credittitle);
+            this.panel1.Location = new System.Drawing.Point(568, 12);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(51000);
+            this.panel1.Size = new System.Drawing.Size(220, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(10, 263);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(157, 20);
+            this.title.TabIndex = 8;
+            this.title.Text = "My Title";
+            // 
+            // compilebtn
+            // 
+            this.compilebtn.Location = new System.Drawing.Point(58, 391);
+            this.compilebtn.Name = "compilebtn";
+            this.compilebtn.Size = new System.Drawing.Size(118, 26);
+            this.compilebtn.TabIndex = 1;
+            this.compilebtn.Text = "Compile";
+            this.compilebtn.UseVisualStyleBackColor = true;
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(10, 289);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(199, 72);
+            this.description.TabIndex = 7;
+            this.description.Text = "My description";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(16, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 131);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "This project was inspired by fchb1239s GorillaTexturepacks preview video. By pres" +
+    "sing the \"Compile\" button below, you agree that this texture will not offer a in" +
+    " game advantage.";
+            // 
+            // credittitle
+            // 
+            this.credittitle.AutoSize = true;
+            this.credittitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.credittitle.Location = new System.Drawing.Point(55, 12);
+            this.credittitle.Name = "credittitle";
+            this.credittitle.Size = new System.Drawing.Size(121, 26);
+            this.credittitle.TabIndex = 5;
+            this.credittitle.Text = "Credits, Information \r\nand Legal";
+            // 
+            // makerzonetitle
+            // 
+            this.makerzonetitle.AutoSize = true;
+            this.makerzonetitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.makerzonetitle.Location = new System.Drawing.Point(12, 9);
+            this.makerzonetitle.Name = "makerzonetitle";
+            this.makerzonetitle.Size = new System.Drawing.Size(168, 31);
+            this.makerzonetitle.TabIndex = 1;
+            this.makerzonetitle.Text = "Maker Zone";
+            // 
+            // ground
+            // 
+            this.ground.AutoSize = true;
+            this.ground.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.ground.Location = new System.Drawing.Point(85, 62);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(64, 20);
+            this.ground.TabIndex = 3;
+            this.ground.Text = "Ground";
+            // 
+            // primaryatlas
+            // 
+            this.primaryatlas.AutoSize = true;
+            this.primaryatlas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.primaryatlas.Location = new System.Drawing.Point(70, 192);
+            this.primaryatlas.Name = "primaryatlas";
+            this.primaryatlas.Size = new System.Drawing.Size(110, 20);
+            this.primaryatlas.TabIndex = 5;
+            this.primaryatlas.Text = "Primary Atlas";
+            // 
+            // secondaryatlas
+            // 
+            this.secondaryatlas.AutoSize = true;
+            this.secondaryatlas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.secondaryatlas.Location = new System.Drawing.Point(218, 63);
+            this.secondaryatlas.Name = "secondaryatlas";
+            this.secondaryatlas.Size = new System.Drawing.Size(96, 20);
+            this.secondaryatlas.TabIndex = 7;
+            this.secondaryatlas.Text = "Tree Stump";
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(18, 415);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(75, 23);
+            this.savebtn.TabIndex = 8;
+            this.savebtn.Text = "Save";
+            this.savebtn.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(218, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tree Room";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Application.Properties.Resources.treeroomatlas;
+            this.pictureBox3.InitialImage = global::Application.Properties.Resources.forestatlas;
+            this.pictureBox3.Location = new System.Drawing.Point(207, 215);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(123, 97);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Application.Properties.Resources.Tree_Texture_Baker_mat__MainTex_atlas_0;
+            this.pictureBox2.InitialImage = global::Application.Properties.Resources.forestatlas;
+            this.pictureBox2.Location = new System.Drawing.Point(207, 85);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 97);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // forestatlaspriview
+            // 
+            this.forestatlaspriview.Image = global::Application.Properties.Resources.forestatlas;
+            this.forestatlaspriview.InitialImage = global::Application.Properties.Resources.forestatlas;
+            this.forestatlaspriview.Location = new System.Drawing.Point(57, 215);
+            this.forestatlaspriview.Name = "forestatlaspriview";
+            this.forestatlaspriview.Size = new System.Drawing.Size(123, 97);
+            this.forestatlaspriview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.forestatlaspriview.TabIndex = 4;
+            this.forestatlaspriview.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(57, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // isverfiedbtn
+            // 
+            this.isverfiedbtn.AutoSize = true;
+            this.isverfiedbtn.Location = new System.Drawing.Point(19, 368);
+            this.isverfiedbtn.Name = "isverfiedbtn";
+            this.isverfiedbtn.Size = new System.Drawing.Size(72, 17);
+            this.isverfiedbtn.TabIndex = 9;
+            this.isverfiedbtn.Text = "Is Verified";
+            this.isverfiedbtn.UseVisualStyleBackColor = true;
+            // 
+            // MakerWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.secondaryatlas);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.primaryatlas);
+            this.Controls.Add(this.forestatlaspriview);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.makerzonetitle);
+            this.Controls.Add(this.panel1);
+            this.Name = "MakerWindow";
+            this.Text = "Texture Compiler";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.forestatlaspriview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox description;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label credittitle;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Button compilebtn;
+        private System.Windows.Forms.Label makerzonetitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label ground;
+        private System.Windows.Forms.Label primaryatlas;
+        private System.Windows.Forms.PictureBox forestatlaspriview;
+        private System.Windows.Forms.Label secondaryatlas;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.CheckBox isverfiedbtn;
+    }
+}
---
+++ Application/Application/MakerWindow.cs
@@ -0,0 +1,210 @@
+﻿/*
+    TODO: Make the title and description save feature actaully work. 
+    When release add verification
+*/
+
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Windows.Forms;
+using TextureLoader.Models;
+
+namespace App
+{
+    public partial class MakerWindow : Form
+    {
+        public MakerWindow()
+        {
+            InitializeComponent();
+            LoadPresets();
+
+            savebtn.Click += Savebtn_Click;
+            pictureBox1.Click += PictureBox1_Click; // ground
+            forestatlaspriview.Click += Forestatlaspriview_Click;
+            pictureBox2.Click += PictureBox2_Click;
+            pictureBox3.Click += PictureBox3_Click;
+            isverfiedbtn.Click += Isverfiedbtn_Click;
+
+            compilebtn.Click += Compilebtn_Click;
+        }
+
+        private void Isverfiedbtn_Click(object sender, EventArgs e)
+        {
+            if (GetProject.DeveloperToken != "IWouldHideThisButIamLazy since most people here are 5 it will probably be fine.")
+            {
+                MessageBox.Show("Invalid Input", "You are not a verified developer.", MessageBoxButtons.OK);
+                isverfiedbtn.Checked = false;
+                return;
+            }
+        }
+
+        private void Compilebtn_Click(object sender, EventArgs e)
+        {
+            SaveProject(false);
+
+            string TEMPDirectory = Directory.GetCurrentDirectory() + "/TEMP";
+            if (Directory.Exists(TEMPDirectory))
+                Directory.Delete(TEMPDirectory, true);
+            Directory.CreateDirectory(TEMPDirectory);
+
+            string path = GetDirectory();
+
+            Package package = new Package(GetProject.Name, GetProject.Description, isverfiedbtn.Checked);
+            string json = JsonConvert.SerializeObject(package);
+            File.WriteAllText(TEMPDirectory + "/package.json", json);
+            File.Copy(GetProject.Ground, TEMPDirectory + "/ground.png");
+            File.Copy(GetProject.MainAtlas, TEMPDirectory + "/atlas.png");
+            File.Copy(GetProject.StumpAtlas, TEMPDirectory + "/treestump.png");
+            File.Copy(GetProject.StumpRoom, TEMPDirectory + "/treestumproom.png");
+
+            string ZIPPath = path + GetProject.Name + ".texture";
+            if (File.Exists(ZIPPath))
+                File.Delete(ZIPPath);
+
+            ZipFile.CreateFromDirectory(TEMPDirectory, ZIPPath);
+            Directory.Delete(TEMPDirectory, true);
+
+            MessageBox.Show("Compile", "Successfully compiled your currently project data.", MessageBoxButtons.OK);
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+            string path = GetFile();
+            if (path != null)
+            {
+                pictureBox3.ImageLocation = path;
+                GetProject.StumpRoom = path;
+            }
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            string path = GetFile();
+            if (path != null)
+            {
+                pictureBox2.ImageLocation = path;
+                GetProject.StumpAtlas = path;
+            }
+        }
+
+        private void Forestatlaspriview_Click(object sender, EventArgs e)
+        {
+            string path = GetFile();
+            if (path != null)
+            {
+                forestatlaspriview.ImageLocation = path;
+                GetProject.MainAtlas = path;
+            }
+        }
+
+        private void Savebtn_Click(object sender, EventArgs e)
+        {
+            SaveProject(true);
+        }
+
+        // Ground
+        private void PictureBox1_Click(object sender, System.EventArgs e)
+        {
+            string path = GetFile();
+            if (path != null)
+            {
+                pictureBox1.ImageLocation = path;
+                GetProject.Ground = path;
+            }
+        }
+
+        // Util
+        private void LoadPresets()
+        {
+            MakerProject makerProject = GetProject;
+            pictureBox1.ImageLocation = makerProject.Ground;
+            pictureBox2.ImageLocation = makerProject.StumpAtlas;
+            forestatlaspriview.ImageLocation = makerProject.MainAtlas;
+
+            title.Text = makerProject.Name;
+            description.Text = makerProject.Description;
+        }
+
+        private MakerProject _project;
+        private MakerProject GetProject
+        {
+            get
+            {
+                if (_project == null)
+                {
+                    string path = Directory.GetCurrentDirectory() + "/project.json";
+                    if (!File.Exists(path))
+                        File.WriteAllText(path, JsonConvert.SerializeObject(new MakerProject()));
+                    _project = JsonConvert.DeserializeObject<MakerProject>(File.ReadAllText(path));
+                }
+                return _project;
+            }
+            set
+            {
+                _project = value;
+            }
+        }
+        private void SaveProject(bool ShowMessageBox = false)
+        {
+            string Json = JsonConvert.SerializeObject(GetProject);
+            string path = Directory.GetCurrentDirectory() + "/project.json";
+
+            GetProject.Name = title.Text;
+            GetProject.Description = description.Text;
+
+            File.WriteAllText(path, Json);
+            if (ShowMessageBox)
+                MessageBox.Show("Save Project", "Successfully saved your currently project data.", MessageBoxButtons.OK);
+        }
+
+        private string GetDirectory()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.SelectedPath = Directory.GetCurrentDirectory();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                return folderBrowserDialog.SelectedPath + "/";
+            }
+            return null;
+        }
+        private string GetFile(string path = "")
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = path == "" ? Directory.GetCurrentDirectory() : path;
+            openFileDialog.Filter = "Image Files (*.png)|*.png";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.CheckFileExists = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog.FileName;
+            }
+            return null;
+        }
+    }
+
+    [Serializable]
+    public class MakerProject
+    {
+        public string Name;
+        public string Description;
+
+        public string Ground;
+        public string MainAtlas;
+        public string StumpAtlas;
+        public string StumpRoom;
+
+        public string DeveloperToken;
+
+        public MakerProject()
+        {
+            Name = "My Title";
+            Description = "My description";
+            Ground = "";
+            MainAtlas = "";
+            StumpAtlas = "";
+            StumpRoom = "";
+        }
+    }
+}
